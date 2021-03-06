7a3b03847a5b85d1af0baa3b039fe6728f8e0d8d
wanjugucode <karobiaanastasia.com>
2021-03-06 15:25:46 +0300

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+function fruit_list (){
+    var fruits=("waterMelon","grapes","pineapple","strawberries");
+    fruit_list.array.forEach(element => {
+        
+    });
+    console.log (fruits)
+
+}
+fruit_list();
